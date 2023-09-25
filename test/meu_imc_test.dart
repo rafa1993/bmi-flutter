90ad120a52c9f1a4a708e7b23af383ceb61da7e6
rafaelaMorais <rafaelahmorais@hotmail.com>
2023-09-25 20:29:11 -0300

    imc: test

--- test/meu_imc_test.dart
+++ test/meu_imc_test.dart
@@ -1,8 +1,16 @@
+import 'package:meu_imc/calculator.dart';
 import 'package:meu_imc/meu_imc.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('Assert correct calculation', () {
+
+  // checks normal weight
+  var height = 182;
+  var weight = 76;  
+  
+   var bmi = BMICalculator(height: height, weight: weight);
+
+  expect(bmi, '22.94');  
   });
 }
